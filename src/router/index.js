401dcfcebb4ac070644045875551bfd8fda155aa
honhonni <725586030@qq.com>
2024-09-02 14:26:16 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import store from '@/store'
+
+// 路由规则
+const routes = []
+
+// vue2.0 new VueRouter({}) 创建路由实例
+// vue3.0 createRouter({}) 创建路由实例
+const router = createRouter({
+  // 使用hash的路由模式
+  history: createWebHashHistory(),
+  routes,
+  // 每次切换路由的时候滚动到页面顶部
+  scrollBehavior() {
+    // vue2.0  x  y  控制
+    // vue3.0  left  top 控制
+    return { left: 0, top: 0 }
+  }
+})
+
+// 前置导航守卫
+router.beforeEach((to, from, next) => {
+  // 需要登录的路由：地址是以 /member 开头
+  const { profile } = store.state.user
+  if (!profile.token && to.path.startsWith('/member')) {
+    return next('/login?redirectUrl=' + encodeURIComponent(to.fullPath))
+  }
+  next()
+})
+
+export default router
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import { createStore } from 'vuex'
+
+// 三个模块
+
+export default createStore({})
